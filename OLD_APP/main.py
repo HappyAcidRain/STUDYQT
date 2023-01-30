fe67677903a1fd5a25bb94539fb07bfcc97e0446
HappyAcidRain <Sashaklivsovpechka@gmail.com>
2023-01-30 22:47:02 +0300

    aded some

---
+++ OLD_APP/main.py
@@ -0,0 +1,101 @@
+from PyQt5 import QtWidgets
+import sys
+import subprocess
+import mainWin
+import setings
+import sqlite3
+
+
+class ConfPart(QtWidgets.QMainWindow, setings.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # подключаем кнопку
+        self.btn_save.clicked.connect(self.save_data)
+        self.btn_save.setText("Сохранить!")
+
+        #пути в полях (placeholder'ы)
+
+        # подключаемся к БД
+        connect = sqlite3.connect("OLD_APP\conf.db")
+        cursor = connect.cursor()
+
+        # выборка данных (IDE_way)
+        cursor.execute("SELECT IDE_way FROM settings WHERE rowid = 1")
+        ide_way = str(cursor.fetchone())
+
+        # выборка данных (browser_way)
+        cursor.execute("SELECT browser_way FROM settings WHERE rowid = 1")
+        browser_way = str(cursor.fetchone())
+
+        # устанавливаем placeholder'ы
+        self.le_IDEway.setPlaceholderText(ide_way)
+        self.le_browserWay.setPlaceholderText(browser_way)
+
+        # закрываем подключение к БД
+        connect.close()
+
+    # кнопка сохранения
+    def save_data(self):
+        # подключаемся к БД
+        connect = sqlite3.connect("OLD_APP\conf.db")
+        cursor = connect.cursor()
+
+        # получение данных с полей ввода
+        ide_way = self.le_IDEway.text()
+        browser_way = self.le_browserWay.text()
+
+        # запись в БД
+        cursor.execute(f"UPDATE settings SET IDE_way = '{ide_way}' WHERE rowid = 1 ")
+        cursor.execute(f"UPDATE settings SET browser_way = '{browser_way}' WHERE rowid = 1 ")
+        connect.commit()
+
+        connect.close()
+        print("DATA SAVED !")
+
+
+class MainPart(QtWidgets.QMainWindow, mainWin.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    # основное окно
+    def main_window(self):
+        # кнопки
+        self.btn_start.clicked.connect(self.start_app)
+        self.btn_conf.clicked.connect(self.conf_window)
+
+    # окно настроек
+    def conf_window(self):
+        self.config_window = ConfPart()
+        self.config_window.show()
+
+    # запуск приложений
+    def start_app(self):
+        # подключаемся к БД
+        connect = sqlite3.connect("OLD_APP\conf.db")
+        cursor = connect.cursor()
+
+        # выборка данных (IDE_way)
+        cursor.execute("SELECT IDE_way FROM settings WHERE rowid = 1")
+        ide_way = cursor.fetchone()
+
+        # выборка данных (browser_way)
+        cursor.execute("SELECT browser_way FROM settings WHERE rowid = 1")
+        browser_way = cursor.fetchone()
+
+        # открываем приложения
+        subprocess.Popen(ide_way)
+        subprocess.Popen(browser_way)
+
+        # закрываем подключение к БД
+        connect.close()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainPart()
+    w.show()
+    w.main_window()
+    sys.exit(app.exec_())
---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+import pageOne
+import pageTwo
+
+
+class MainWindow(QtWidgets.QMainWindow, pageOne.Ui_MainWindow, QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.btn_next.clicked.connect(self.nextPage)
+
+    def nextPage(self):
+        screen2 = Screen2()
+        w.addWidget(screen2)
+        w.setCurrentIndex(w.currentIndex()+1)
+
+
+class Screen2(QtWidgets.QMainWindow, pageTwo.Ui_MainWindow, QDialog):
+    def __init__(self):
+        super(Screen2, self).__init__()
+        self.setupUi(self)
+
+        self.btn_back.clicked.connect(self.pervPage)
+
+    def pervPage(self):
+        w.setCurrentIndex(w.currentIndex()-1)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    w = QtWidgets.QStackedWidget()
+    w.addWidget(mainWindow)
+
+    w.setFixedHeight(93)
+    w.setFixedWidth(186)
+
+    w.show()
+    sys.exit(app.exec_())
